b8a9f3f7e3279ce82213b4027c07481d2adeab6f
Rishikesh Nair <rishikesh@example.com>
2020-09-29 19:23:54 +0530

    Add registration of player based on name

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,56 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import socketIOClient from "socket.io-client";
-const ENDPOINT = "http://localhost:4000";
 
-const socket = socketIOClient(ENDPOINT);
+const ENDPOINT = "http://localhost:4000";
+let socket;
 
 function App() {
   const [word, setWord] = useState('')
   const [story, setStory] = useState('')
+  const [isRegistered, setIsRegistered] = useState(false)
+  const [playerName, setPlayerName] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+
+  useEffect(() => {
+    socket = socketIOClient(ENDPOINT);
+    socket.on('story', response => {
+      setStory(response)
+    })
+  })
+
+  const registerPlayer = () => {
+    socket.emit('register', {name: playerName})
+    socket.on('registered', data => {
+      if(data.ok) {
+        console.log('registered successfuly')
+        setErrorMessage('')
+        setIsRegistered(true)
+      } else {
+        setErrorMessage(data.message)
+        setIsRegistered(false)
+      }
+    })
+  }
+
+  const unRegisterPlayer = () => {
+    socket.emit('un_register', {name: playerName})
+    socket.on('un_registered', data => {
+      if(data.ok) {
+        console.log('unregistered successfuly')
+        setErrorMessage('')
+      } else {
+        setErrorMessage(data.message)
+        setIsRegistered(false)
+      }
+    })
+  }
+
+  const handleRegisterInput = (e) => {
+    let value = e.target.value;
+    if (value.trim() !== "" || value !== null || value !== undefined) {
+      setPlayerName(value)
+    }
+  }
 
   const handleInput = (e) => {
     let value = e.target.value;
@@ -19,7 +63,6 @@ function App() {
     socket.emit('add_word', word)
     socket.on('story', response => {
       setStory(response)
-      console.log(story)
     })
   }
 
@@ -27,19 +70,28 @@ function App() {
     socket.emit('reset_story', "")
     socket.on('story', response => {
       setStory(response)
-      console.log(story)
     })
   }
 
   return (
     <div>
-      <p>{socket.id}</p>
-      <h1>{story}</h1>
-      <input type="text" name="word" onChange={handleInput}></input>
-      <button onClick={submitWord}>Submit</button>
-      <button onClick={resetStory}>Reset</button>
+      { isRegistered ?
+        <div>
+          <p>Player name: {playerName}</p>
+          <h1>{story}</h1>
+          <input type="text" name="word" onChange={handleInput}></input>
+          <button onClick={submitWord}>Submit</button>
+          <button onClick={resetStory}>Reset</button>
+        </div>
+        :
+        <div>
+          <input type="text" name="word" onChange={handleRegisterInput}></input>
+          <button onClick={registerPlayer}>Join</button>
+          <button onClick={unRegisterPlayer}>Unregister</button>
+          <p>{errorMessage}</p>
+        </div>
+      }
     </div>
-      
   );
 }
 
--- server/app.js
+++ server/app.js
@@ -2,19 +2,58 @@ const io = require('socket.io')();
 
 let story = ""
 
-io.on('connection', client => { 
+let clients = []
+
+io.on('connection', client => {
+  io.emit('story', story)
   // console.log(client)
-  client.on('add_word', (word) => { 
+  client.on('add_word', (word) => {
     story += ` ${word}`;
     console.log(story)
     io.emit('story', story)
   });
 
-  client.on('reset_story', (word) => { 
+  client.on('reset_story', (word) => {
     story = word;
     console.log(story)
     io.emit('story', story)
   });
+
+  client.on('register', data => {
+    if (!clients.some(x => x.name === data.name)) {
+      clients.push({
+        name: data.name,
+        id: client.id
+      })
+      client.emit('registered', {
+        ok: true
+      })
+    } else {
+      // name already exists
+      client.emit('registered', {
+        ok: false,
+        message: 'Client already exists.'
+      })
+    }
+    console.log("Total clients: ", JSON.stringify(clients))
+  })
+
+  client.on('un_register', data => {
+    let index = clients.findIndex(x => x.name === data.name)
+    if(index !== -1) {
+      clients.splice(index, 1);
+      client.emit('un_registered', {
+        ok: true
+      })
+    } else {
+      // name not found
+      client.emit('un_registered', {
+        ok: false,
+        message: 'Client not found.'
+      })
+    }
+    console.log("Total clients: ", JSON.stringify(clients))
+  })
 });
 
 io.listen(4000);
